ebee14da9cb10077ae37803a5d657c968b279644
Liulongge <792286891@qq.com>
2020-11-29 18:29:47 +0800

    string

---
+++ format.py
@@ -0,0 +1,12 @@
+a = 3.1415926
+print("%d , %f" % (a,a))
+print("%s" % (a))
+print("%E " % (a))
+print("%x" %(10))
+
+print("%3.2f" %(a))
+print("%+3.2f" %(3.14159))
+print("\\v")
+
+print("{0} love {1}".format("I","you"))
+print("{a} love {b}".format(a = "I", b = "you"))
