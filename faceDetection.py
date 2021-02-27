643d11ae0089116aff9aa50e47f745d44ab96770
RungeLiu <792286891@qq.com>
2021-02-28 00:31:47 +0800

    Update faceDetection.py

--- faceDetection.py
+++ faceDetection.py
@@ -1,9 +1,32 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier(r"D:/OpenCV4430/opencv/sources/data/haarcascades/haarcascade_frontalface_default.xml")
-eye_cascade = cv2.CascadeClassifier(r"D:/OpenCV4430/opencv/sources/data/haarcascades/haarcascade_eye_tree_eyeglasses.xml")
+camera = cv2.VideoCapture(0)
+cv2.namedWindow('MyCamera')
+face_cascade = cv2.CascadeClassifier(r"E:/ProgramData/Anaconda3/Lib/site-packages/cv2/data/haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(r"E:/ProgramData/Anaconda3/Lib/site-packages/cv2/data/haarcascade_eye_tree_eyeglasses.xml")
 
-img = cv2.imread("D:\\myCode\\peaple.jpg")
+while True:
+    success, frame = camera.read()
+    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+
+
+    for x, y, w, h in faces:
+        img = cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0),2)
+
+        face_area = img[y: y+h, x: x+w]
+        eyes = eye_cascade.detectMultiScale(face_area)
+
+        for ex, ey, ew, eh in eyes:
+            cv2.rectangle(face_area, (ex,ey), (ex + ew, ey + eh),(0, 255, 0),1)
+    cv2.imshow('MyCamera',frame)
+    if cv2.waitKey(1) & 0xff == ord('q'):
+        break
+
+cv2.destroyWindow('MyCamera')
+camera.release()
+
+'''
+img = cv2.imread("C:\\Users\\runge.liu\\Desktop\\test.jpg")
 
 faces = face_cascade.detectMultiScale(img, 1.3, 5)
 
@@ -21,3 +44,4 @@ cv2.imshow("im3", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 cv2.imwrite("output.jpg", img)
+'''
